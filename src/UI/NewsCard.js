1d8f6814e5129e9335ccfa9c11b8f00d656d627e
Ethan Klukkert <ethan7klukkert@gmail.com>
2022-11-28 22:37:15 -0500

    news card styles

--- src/UI/NewsCard.js
+++ src/UI/NewsCard.js
@@ -1,13 +1,21 @@
 import classes from "./NewsCard.module.css"
 
 const NewsCard = (props) => {
-  const image = props.imageLink ? <img src={props.imageLink} /> : "";
+
+  let description = props.description
+
+  if (description.charAt(description.length - 1) !== ".") {
+    description += " ..."
+  }
+
+  const image = props.imageLink ? <img src={props.imageLink} className={classes.image} /> : "";
 
   return (
     <div className={classes.card}>
       {image}
-      <h3>{props.name}</h3>
-      <p>{props.description}</p>
+      <p className={classes.name}>{props.name}</p>
+      <hr className={classes.divider} />
+      <p className={classes.description}>{description}</p>
     </div>
   );
 };
