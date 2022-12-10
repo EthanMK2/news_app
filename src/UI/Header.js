c1fc071f19b777ed422d7443682f6d427620c8a3
Ethan Klukkert <ethan7klukkert@gmail.com>
2022-12-09 22:29:56 -0500

    header links styles

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import classes from "./Header.module.css"
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
@@ -8,39 +8,41 @@ const Header = () => {
       <ul className={classes["header-nav"]}>
         <li className={classes["header-home"]}>
           <Link to="/" className={classes["header-link"]}>
-            <h1>Headline News</h1>
+            <h1>News Website</h1>
           </Link>
         </li>
-        <li className={classes["header-nav-li"]}>
-          <Link to="/" className={classes["header-link"]}>
+
+        <Link to="/" className={classes["header-link"]}>
+          <li className={classes["header-nav-li"]}>
             <p>Entertainment</p>
-          </Link>
-        </li>
-        <li className={classes["header-nav-li"]}>
-          <Link to="/" className={classes["header-link"]}>
+          </li>
+        </Link>
+        <Link to="/" className={classes["header-link"]}>
+          <li className={classes["header-nav-li"]}>
             <p>Business</p>
-          </Link>
-        </li>
-        <li className={classes["header-nav-li"]}>
-          <Link to="/" className={classes["header-link"]}>
+          </li>
+        </Link>
+        <Link to="/" className={classes["header-link"]}>
+          <li className={classes["header-nav-li"]}>
             <p>Science</p>
-          </Link>
-        </li>
-        <li className={classes["header-nav-li"]}>
-          <Link to="/" className={classes["header-link"]}>
+          </li>
+        </Link>
+        <Link to="/" className={classes["header-link"]}>
+          <li className={classes["header-nav-li"]}>
             <p>Technology</p>
-          </Link>
-        </li>
-        <li className={classes["header-nav-li"]}>
-          <Link to="/" className={classes["header-link"]}>
+          </li>
+        </Link>
+        <Link to="/" className={classes["header-link"]}>
+          <li className={classes["header-nav-li"]}>
             <p>Sports</p>
-          </Link>
-        </li>
-        <li className={classes["header-nav-li"]}>
-          <Link to="/" className={classes["header-link"]}>
+          </li>
+        </Link>
+        <Link to="/" className={classes["header-link"]}>
+          <li className={classes["header-nav-li"]}>
             <p>Health</p>
-          </Link>
-        </li>
+          </li>
+        </Link>
+
         <li className={classes["profile-link"]}>
           <Link to="/profile" className={classes["header-link"]}>
             <p>Profile</p>
