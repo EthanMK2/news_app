19051abf4716b8685cd4b15b3a14079f3de9c76d
Ethan Klukkert <ethan7klukkert@gmail.com>
2023-02-10 18:44:50 -0500

    footer links

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -12,10 +12,10 @@ const Footer = () => {
         </p>
       </section>
       <section className={classes["footer-links"]}>
-        <a className={classes["linkedin-link"]}>LinkedIn</a>
-        <a className={classes["github-portfolio-link"]}>GitHub</a>
-        <a className={classes["website-github-link"]}>This Site's GitHub</a>
-        <a className={classes["resume-link"]}>Resume</a>
+        <a className={classes["linkedin-link"]} href="https://www.linkedin.com/in/ethanklukkert/" target="_blank">LinkedIn</a>
+        <a className={classes["github-portfolio-link"]} href="https://github.com/EthanMK2" target="_blank">GitHub</a>
+        <a className={classes["website-github-link"]} href="https://github.com/EthanMK2/news_app" target="_blank">This Site's GitHub</a>
+        <a className={classes["resume-link"]} target="_blank">Resume</a>
       </section>
     </footer>
   );
